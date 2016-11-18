b42492e450af3fbeee2948b49a9f74ac9b82f5c3
Hans-Kristian <hkbruvold@gmail.com>
2016-11-18 01:02:32 +0100

    added functions to creeps manager

--- branches/refactoring/main.js
+++ branches/refactoring/main.js
@@ -1,56 +1,10 @@
-var roleSafeHarvester = require ('role.safeharvester');
-var roleSafeUpgrader = require ('role.safeupgrader');
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleRoadworker = require('role.roadworker');
-var roleClaimer = require('role.claimer');
-var roleEnergyStealer = require('role.energystealer');
-var roleRoomer = require('role.roomer');
-var roleDedicatedHarvester = require('role.dedicatedharvester');
-var roleSpawnFiller = require('role.spawnfiller');
-var roleTower = require('role.tower');
-var managerSpawning = require('manager.spawning');
+let mgrCreeps = require("mgr.creeps");
 
 module.exports.loop = function () {
-    /* set to true to set some memory */
-    if (false) {
-        /* mainsrc is array of containers under dedicated harvesters */
-        Game.spawns.Spawn1.room.memory["mainsrc"] = ["582cdb7b051b7a2e782e93a3","582cdddb4e3fd7496e2fb6c5"];
-        
+    let mainRoom = Game.spawns.Spawn1.room.name;
+    if (Game.time % 100 == 0) {
+        mgrCreeps.reset(mainRoom);
     }
-    
-    
-    
-    if (Game.creeps["SAFEMODE_HARVESTER"]) {roleSafeHarvester.run(Game.creeps["SAFEMODE_HARVESTER"]);}
-    if (Game.creeps["SAFEMODE_UPGRADER"]) {roleSafeUpgrader.run(Game.creeps["SAFEMODE_UPGRADER"]);}
-    if (managerSpawning.safe(Game.spawns["Spawn1"]) == false) {
-        managerSpawning.run(Game.spawns["Spawn1"]);
-    }
-    
-    
-    //roleTower.run(Game.getObjectById("582a0533739724f97f69cb8c"));
-    
-    for(let name in Game.creeps) {
-        let creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        } else if (creep.memory.role == 'spawnfiller') {
-            roleSpawnFiller.run(creep);
-        } else if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        } else if (creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        } else if (creep.memory.role == 'roadworker') {
-            roleRoadworker.run(creep);
-        } else if (creep.memory.role == 'claimer') {
-            roleClaimer.run(creep);
-        } else if (creep.memory.role == 'energystealer') {
-            roleEnergyStealer.run(creep);
-        } else if (creep.memory.role == 'roomer') {
-            roleRoomer.run(creep);
-        } else if (creep.memory.role == 'dedicatedharvester') {
-            roleDedicatedHarvester.run(creep);
-        }
-    }
-}
+
+    mgrCreeps.runCreeps();
+};
