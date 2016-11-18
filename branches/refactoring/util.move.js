2233951d64abd1af20b622aa149876501b6fc0a1
Hans-Kristian <hkbruvold@gmail.com>
2016-11-18 11:11:22 +0100

    added move functions

---
+++ branches/refactoring/util.move.js
@@ -0,0 +1,42 @@
+/* This module contains functions to help creeps create paths and follow them */
+module.exports = {
+    createPath, move
+};
+
+function createPath(creep, target) {
+    /* Creates a path from creep to target that the creep can follow by using move() */
+    creep.memory.path = creep.pos.findPathTo(target);
+    creep.memory.pathprog = 0;
+    creep.memory.target = target.id;
+}
+
+function move(creep) {
+    /* Move the creep along the created path. Will regenerate path if hindered.
+    *  Return codes:
+    *  0 - OK.
+    *  1 - path completed.
+    *  2 - had to create new path.
+    *  3 - is tired.
+    *  4 - creep memory does not contain path.*/
+    let path = creep.memory.path;
+    let pathprog = creep.memory.pathprog;
+
+    if (!creep.memory.path) return 4;
+    if (path.length == pathprog) return 1;
+
+    let res = creep.move(path[pathprog]);
+    if (res == OK) {
+        if (creep.pos.x == path[pathprog].x && creep.pos.y == path[pathprog].y) {
+            creep.memory.pathprog = pathprog + 1;
+            return 0;
+        }
+        // Something went wrong, most likely something in the way
+        // Solution: Generate new path
+        let target = Game.getObjectById(creep.memory.target);
+        creep.memory.path = creep.pos.findPathTo(target);
+        creep.memory.pathprog = 0;
+    } else if (res == ERR_TIRED) {
+        creep.memory.tiredCound += 1;
+        return 3;
+    }
+}
