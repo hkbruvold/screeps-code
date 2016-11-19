564858c57bd81f18924f6903e7f245c4a11c0eb2
Hans-Kristian <hkbruvold@gmail.com>
2016-11-19 18:49:19 +0100

    added basic spawnfiller module, added function in util.energy

---
+++ branches/refactoring/type.spawnfiller.js
@@ -0,0 +1,10 @@
+/* The spawnfiller transports energy to spawn and extensions */
+module.exports = {run};
+
+function run(creep) {
+    let utilMove = require("util.move");
+    let mgrSpawner = require("mgr.spawner");
+
+
+}
+
--- branches/refactoring/util.energy.js
+++ branches/refactoring/util.energy.js
@@ -1,12 +1,27 @@
 /* This module contains functions to be used by creeps to find energy sources */
 module.exports = {
-    getClosestEnergyContainer, getHarvesterContainer, getRegularStorage, getDroppedResource, getEnergy
+    getClosestEnergyContainer, getHarvesterContainer, getRegularStorage, getDroppedResource, getEnergy, getEnergyDroppedPriority, takeEnergy
 };
 
-function getEnergy() {
-    /* Returns an object to get energy from. Closest container prioritized*/
+function getEnergy(creep) {
+    /* Returns an object to get energy from. Closest container prioritized */
     if (src = getClosestEnergyContainer(creep)) {return src}
     if (src = getDroppedResource(creep)) {return src}
+}
+
+function getEnergyDroppedPriority(creep) {
+    /* Returns an object to get energy from. Dropped resource prioritized */
+    if (src = getDroppedResource(creep)) {return src}
+    if (src = getClosestEnergyContainer(creep)) {return src}
+}
+
+function takeEnergy(creep, object) {
+    /* Make creep execute the correct take command on object */
+    if (object.resourceType == RESOURCE_ENERGY) {
+        return creep.pickup(object);
+    } else {
+        return creep.withdraw(object, RESOURCE_ENERGY);
+    }
 }
 
 function getClosestEnergyContainer(creep) {
@@ -49,8 +64,8 @@ function getDroppedResource(creep) {
     let droppedmem = creep.room.memory.dropped;
     let dropped = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
         filter: (resource) => {
-            return (droppedmed.indexOf(resource.id) == -1 && resource.amount > 50) ||
-                (droppedmem.indexOf(resource.id) >= -1 && resource.amount > droppedmem[droppedmem.indexOf(resource.id)]);
+            return (resource.resourceType == RESOURCE_ENERGY) && ((droppedmed.indexOf(resource.id) == -1 && resource.amount > 50) ||
+                (droppedmem.indexOf(resource.id) >= -1 && resource.amount > droppedmem[droppedmem.indexOf(resource.id)]));
         }
     });
     if (dropped) {
