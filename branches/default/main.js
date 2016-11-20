dcba4e41a4d1de74df28fc2fd57f73c8f6696bfe
Hans-Kristian <hkbruvold@gmail.com>
2016-11-20 02:19:19 +0100

    fixed a bit in main

--- branches/default/main.js
+++ branches/default/main.js
@@ -13,9 +13,9 @@ module.exports.loop = function () {
     /* Run initial room configs for rooms not initialized (can be commented out to save CPU)*/
     for (let i in rooms) {
         if (rooms[i].memory.initialized != true) {
-            mgrMemory.initSpawnMemory(Game.spawns[confRooms[rooms[0].name].spawners[0]]);
-            mgrSpawner.fillSpawnQueue(Game.spawns[confRooms[rooms[0].name].spawners[0]]);
-            mgrSpawner.recalculateCapacity(Game.spawns[confRooms[rooms[0].name].spawners[0]]);
+            mgrMemory.initSpawnMemory(Game.spawns[confRooms[rooms[i].name].spawners[0]]);
+            mgrSpawner.fillSpawnQueue(Game.spawns[confRooms[rooms[i].name].spawners[0]]);
+            mgrSpawner.recalculateCapacity(Game.spawns[confRooms[rooms[i].name].spawners[0]]);
             mgrMemory.initHarvesterContainers(rooms[i]);
             rooms[i].memory.initialized = true;
         }
@@ -24,16 +24,18 @@ module.exports.loop = function () {
     /* Do some operations every 15 ticks */
     if (Game.time % 15 == 0) {
         for (let i in rooms) {
-            mgrSpawner.fillSpawnQueue(Game.spawns[confRooms[rooms[0].name].spawners[0]]);
-            mgrSpawner.recalculateCapacity(Game.spawns[confRooms[rooms[0].name].spawners[0]]);
+            mgrSpawner.fillSpawnQueue(Game.spawns[confRooms[rooms[i].name].spawners[0]]);
+            mgrSpawner.recalculateCapacity(Game.spawns[confRooms[rooms[i].name].spawners[0]]);
             mgrMemory.initHarvesterContainers(rooms[i]); // Check for new harvester containers (can be commented out if not needed)
         }
         mgrMemory.clearCreepMemory();
     }
 
     /* Spawn safemode harvester/upgrader if needed, otherwise spawn regular creeps */
-    if (!safeMode(Game.spawns[confRooms[rooms[0].name].spawners[0]])) {
-        mgrSpawner.spawnNext(Game.spawns[confRooms[rooms[0].name].spawners[0]]);
+    for (let i in rooms) {
+        if (!safeMode(Game.spawns[confRooms[rooms[i].name].spawners[0]])) {
+            mgrSpawner.spawnNext(Game.spawns[confRooms[rooms[i].name].spawners[0]]);
+        }
     }
 
     /* Run creeps modules */
